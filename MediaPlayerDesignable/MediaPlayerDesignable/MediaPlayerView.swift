93f9b603cc217404cb892cf2aa102d7405bad41a
vincenzo <v.stira@gmail.com>
2016-02-26 18:25:41 +0100

    Image inside layer not working

--- MediaPlayerDesignable/MediaPlayerDesignable/MediaPlayerView.swift
+++ MediaPlayerDesignable/MediaPlayerDesignable/MediaPlayerView.swift
@@ -15,9 +15,25 @@ class MediaPlayerView: UIView {
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
-        let p = UIBezierPath(ovalInRect: CGRectMake(0,0,100,100))
-        UIColor.blueColor().setFill()
-        p.fill()
+        // Drawing code
+        let circleLayer = CAShapeLayer()
+        let circle = UIBezierPath(ovalInRect: CGRectMake(15,30,150,150))
+        //UIColor.blueColor().setFill()
+        //circle.fill()
+        circleLayer.path = circle.CGPath
+        circleLayer.fillColor = UIColor(patternImage: UIImage(named: "cattedrale")!).CGColor
+        layer.addSublayer(circleLayer)
+        
+        let bannerLayer = CAShapeLayer()
+        let banner = UIBezierPath(rect: CGRectMake(circle.bounds.width/2, circle.bounds.height/2, self.bounds.width, 50))
+        //UIColor.redColor().setFill()
+        //banner.fill()
+        bannerLayer.path = banner.CGPath
+        bannerLayer.fillColor = UIColor(red: 0.980, green: 0.878, blue: 0.588, alpha: 1.00).CGColor
+        layer.addSublayer(bannerLayer)
+        
+        circleLayer.zPosition = 2
+        bannerLayer.zPosition = 1
     }
     
 
